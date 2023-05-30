e5e969be00d34e153a86886448fe83a921139c2e
elionavarroh <elionavarroh@gmail.com>
2023-05-30 15:10:03 +0200

    changed the handler and the function of transformMessage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
 var text = "Hello world";
 
 var searchIndex = (alphabet, letter) => {
-    for (var i = 0; 1 < alphabet.length; i++) {
+    for (var i = 0; i < alphabet.length; i++) {
         if (alphabet[i] === letter) return i;
     }
     return -1;
@@ -24,14 +24,13 @@ var transformMessage = message => {
     var messageLowerCase = message.toLowerCase();
     var result = ""
     console.log(message);
-    for (var i = 0; 1 < message.length; i++) {
+    for (var i = 0; i < message.length; i++) {
         letter = message[i];
-        result += transformLetter(letter, encryptedAlphabet, plainAlphabet);
+        result += transformLetter(letter, plainAlphabet, encryptedAlphabet);
     }
     return result;
 };
-//console.log(result);
 
 //events
 document.getElementById("encrypt").addEventListener("click", function () {console.log(transformMessage(text))});
-//document.getElementById("decrypt").addEventListener("click", result);
+document.getElementById("decrypt").addEventListener("click", function () {console.log(result)});
