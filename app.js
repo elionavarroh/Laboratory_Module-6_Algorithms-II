e2de2bd6cc6259e0a2276939f4feda7ea7a54120
elionavarroh <elionavarroh@gmail.com>
2023-06-01 19:06:30 +0200

    add and changed new events for encrypt and decrypt message. Add a new function for decrypt the message

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
 var encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
 
 var text = "Hello world";
+var incoming = document.getElementById("incoming");
+var outcoming = document.getElementById("outcoming");
 
 var searchIndex = (alphabet, letter) => {
     for (var i = 0; i < alphabet.length; i++) {
@@ -21,7 +23,6 @@ var transformLetter = (letter, origin, destiny) => {
 };
 
 var transformMessage = message => {
-    var messageLowerCase = message.toLowerCase();
     var result = ""
     console.log(message);
     for (var i = 0; i < message.length; i++) {
@@ -31,6 +32,22 @@ var transformMessage = message => {
     return result;
 };
 
+var decryptMessage = message => {
+    var result = ""
+    console.log(message);
+    for (var i = 0; i < message.length; i++) {
+        letter = message[i];
+        result += transformLetter(letter, encryptedAlphabet, plainAlphabet);
+    }
+    return result;
+};
+
 //events
-document.getElementById("encrypt").addEventListener("click", function () {console.log(transformMessage(text))});
-document.getElementById("decrypt").addEventListener("click", function () {console.log(result)});
+document.getElementById("encrypt").addEventListener("click", function () {
+    var result = transformMessage(incoming.value);
+    outcoming.value = result;
+});
+document.getElementById("decrypt").addEventListener("click", function () {
+    var result = decryptMessage(outcoming.value);
+    incoming.value = result;
+});
